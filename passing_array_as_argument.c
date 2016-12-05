dfccc3846c6b9e486ca27f8db75336fb2d0c4610
ATONU GHOSH <atonughosh@outlook.com>
2016-12-05 10:47:13 +0530

    Add files via upload
    passing array as argument source code added

---
+++ passing_array_as_argument.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+//function to calculate sum of elements in array
+int SOE(int* A, int size){
+    int i;
+    int total=0;
+    for(i=0;i<size;i++){
+        total+=A[i];
+    }
+    return total;
+}
+
+void main(){
+    int a[] = {5,8,2,5,8,9};
+    int size = sizeof(a)/sizeof(a[0]); //calculating no. of. elements of array
+    int sum = SOE(a,size);
+    printf("The sum of the elements of the array is = %d\n",sum);
+}
