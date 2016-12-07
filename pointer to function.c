ad2422d695b88f516e3eee063b08662c423f588a
ATONU GHOSH <atonughosh@outlook.com>
2016-12-07 10:26:23 +0530

    Add files via upload
    Pointer to function
    This seldom be used as it is logically wrong to use such return of address of a local variable by the called function.

---
+++ pointer to function.c
@@ -0,0 +1,17 @@
+/*
+Program may or may not provide the expected sum of 14
+It depends on the system as the value at the address returned by the
+function add, may be over written by some other function.
+To check this try calling some other function before the print statement.
+*/
+#include<stdio.h>
+int* add(int* a,int* b){
+    int sum = (*a) + (*b);
+    return &sum; //this causes the compiler to issue warning about returning address of local variable
+}
+
+void main(){
+    int x = 10, y = 4;
+    int* ptr = add(&x,&y);
+    printf("The sum of 10 and 4 is = %d",*ptr);
+}
