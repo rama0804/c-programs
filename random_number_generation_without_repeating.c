8b051f25a10e2ffba8e9f977749ff9cf00a070f2
ATONU GHOSH <atonughosh@outlook.com>
2016-12-07 21:25:41 +0530

    Add files via upload

---
+++ random_number_generation_without_repeating.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<time.h>
+void main(){
+    int num[5];
+    int i,j,temp;
+
+    srand(time(NULL));
+    for(i=0;i<5;i++){
+        num[i]=i+1;
+    }
+    for(i=0;i<5;i++){
+        j = (rand()%5)+1;
+        temp = num[i];
+        num[i] = num[j];
+        num[j] = temp;
+    }
+    for(i=0;i<3;i++){
+        printf("%d \n",num[i]);
+    }
+}
